9551ea2f3d8f78006e85c6030df943d6ca7d196b
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2021-05-09 22:43:12 -0500

    updated for JUCE

--- fpsdk_20210311/source/c/Osc3_VC/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	Osc3_VC.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
